7a7fe7973db0206207411b383f6ec8738d612675
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-22 14:16:33 +0530

    test - 4

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -14,13 +14,17 @@ import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
 
-@WebServlet(urlPatterns = {"/ai"},
-    initParams = {
+/*@WebServlet(initParams = {
         @WebInitParam(name = MyServiceServlet.PARAM_API_AI_KEY,
         value = "c52536d801de4975a86024f33e87b511")
-    })
+    })*/
 public class MyServiceServlet extends AIServiceServlet {
   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+/**
    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
