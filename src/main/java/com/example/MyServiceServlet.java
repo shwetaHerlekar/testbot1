eb728940cb75ab69c127e264d40a19c079a13e0e
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-22 17:24:33 +0530

    test - 11

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
@@ -21,13 +23,19 @@ public class MyServiceServlet extends AIServiceServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private String session;
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
+			if (request.getParameter("sessionKey").isEmpty()) {
+				session = request.getSession().toString();
+			}
+			else{
+				session = request.getParameter("sessionKey");
+			}
 			AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
 			response.setContentType("text/plain");
 			response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
