3eff9a643129d7317a5ccd478adad8c7ac6aca91
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-22 15:07:10 +0530

    test - 5

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.Console;
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
 import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
@@ -18,12 +21,25 @@ import ai.api.web.AIServiceServlet;
         @WebInitParam(name = MyServiceServlet.PARAM_API_AI_KEY,
         value = "c52536d801de4975a86024f33e87b511")
     })*/
+
 public class MyServiceServlet extends AIServiceServlet {
-  /**
+	
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public static final String PARAM_API_AI_KEY = "apiaiKey";
+
+	private AIDataService aiDataService;
+	  @Override
+	  public void init(ServletConfig config) throws ServletException {
+	    super.init(config);
+
+	    AIConfiguration aiConfig = new AIConfiguration(config.getInitParameter(PARAM_API_AI_KEY));
+	    aiDataService = new AIDataService(aiConfig);
+	  }
+	
 /**
    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    */
@@ -32,7 +48,7 @@ public class MyServiceServlet extends AIServiceServlet {
     try {
       AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
       response.setContentType("text/plain");
-      response.getWriter().append("Hello");
+      response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
       //aiResponse.getResult().getFulfillment().getSpeech()
     } catch (AIServiceException e) {
       System.out.println("Exception accesing API AI");
