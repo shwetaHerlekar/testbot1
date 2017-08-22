1a88608fbec188588d129e8fc9cfa2f023b568c4
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-22 16:03:42 +0530

    test - 7

---
+++ src/main/java/com/example/BotHandlerServlet.java
@@ -0,0 +1,67 @@
+package com.example;
+import java.io.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.AIServiceContext;
+import ai.api.AIServiceContextBuilder;
+import ai.api.AIServiceException;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
+
+
+// [START example]
+@SuppressWarnings("serial")
+public class BotHandlerServlet extends HttpServlet {
+
+	private AIDataService aiDataService;
+	
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    /*PrintWriter out = resp.getWriter();
+    out.println("Hello Servlet!!");*/
+    
+	String sessionId = req.getParameter("sessionId");
+	try{
+	
+	AIConfiguration aiConfig = new AIConfiguration("c17ce92704f14b0f85181127e2f0e6b6");
+	aiDataService = new AIDataService(aiConfig);
+	
+	AIResponse aiResponse = request(req.getParameter("query"), sessionId);
+	resp.setContentType("text/plain");
+    resp.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
+	}
+	
+	catch(Exception e){
+	
+	}
+  }
+  
+  public AIResponse request(AIRequest aiRequest, HttpSession session)
+      throws AIServiceException {
+    return request(aiRequest,
+        (session != null) ? AIServiceContextBuilder.buildFromSessionId(session.getId()) : null);
+	}
+  
+  public AIResponse request(String query, HttpSession session) throws AIServiceException {
+    return request(new AIRequest(query),
+        (session != null) ? AIServiceContextBuilder.buildFromSessionId(session.getId()) : null);
+	}
+  
+  public AIResponse request(AIRequest aiRequest, AIServiceContext serviceContext)
+      throws AIServiceException {
+    return aiDataService.request(aiRequest, serviceContext);
+	}
+	
+	public AIResponse request(String query, String sessionId) throws AIServiceException {
+    return request(new AIRequest(query),
+        (sessionId != null) ? AIServiceContextBuilder.buildFromSessionId(sessionId) : null);
+	}
+  
+}
--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -1,18 +1,10 @@
 package com.example;
 
-import java.io.Console;
 import java.io.IOException;
-
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import ai.api.AIConfiguration;
-import ai.api.AIDataService;
 import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
@@ -23,36 +15,25 @@ import ai.api.web.AIServiceServlet;
     })*/
 
 public class MyServiceServlet extends AIServiceServlet {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static final String PARAM_API_AI_KEY = "c52536d801de4975a86024f33e87b511";
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
+			response.setContentType("text/plain");
+			response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
+			//aiResponse.getResult().getFulfillment().getSpeech()
+		} catch (AIServiceException e) {
+			System.out.println("Exception accesing API AI");
 
-	private AIDataService aiDataService;
-	  @Override
-	  public void init(ServletConfig config) throws ServletException {
-	    super.init(config);
-
-	    AIConfiguration aiConfig = new AIConfiguration(config.getInitParameter(PARAM_API_AI_KEY));
-	    aiDataService = new AIDataService(aiConfig);
-	  }
-	
-/**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    try {
-      AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
-      response.setContentType("text/plain");
-      response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
-      //aiResponse.getResult().getFulfillment().getSpeech()
-    } catch (AIServiceException e) {
-      System.out.println("Exception accesing API AI");
-      
-    }
-  }
+		}
+	}
 }
