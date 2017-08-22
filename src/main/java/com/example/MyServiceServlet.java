0de0764e56b8e3e6b2587182510ea018e6081a66
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-22 17:40:56 +0530

    test - 12

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -27,7 +27,7 @@ public class MyServiceServlet extends AIServiceServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
 			if (request.getParameter("sessionKey").isEmpty()) {
