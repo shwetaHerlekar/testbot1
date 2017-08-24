6b88991fac68e9f3806007bc512f9c4804c055ca
saiba09 <saiba_sheikh@persistent.co.in>
2017-08-24 14:19:21 +0530

    24aug - webhook - 2

--- src/main/java/com/example/MyWebhookServlet.java
+++ src/main/java/com/example/MyWebhookServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.*;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import ai.api.model.Fulfillment;
 import ai.api.web.AIWebhookServlet;
@@ -22,7 +26,24 @@ public class MyWebhookServlet extends AIWebhookServlet  {
 protected void doWebhook(AIWebhookRequest input, Fulfillment output) {
 	// TODO Auto-generated method stub
 	System.out.println(input.getResult().getAction());
-	output.setSpeech(input.getResult().toString());
+	String action = input.getResult().getAction();
+	HashMap<String, JsonElement> parameter = input.getResult().getParameters();
+	switch (action) {
+	case "overtime":
+	case "overtime_federal":
+	case "overtime_federal_more_yes":
+	case "overtime_states":
+	case "overtime_states_no":
+	case "compliance_expert_yes":
+	case "query":
+	case "state_laws": 
+		output.setSpeech("from Webhook");
+		break;
+
+	default:
+		break;
+	}
+	//output.setSpeech(input.getResult().toString());
 	
 }
 
