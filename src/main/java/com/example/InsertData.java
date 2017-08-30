016716e18411ea8aba4cfc46077e377c59dc89b6
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-30 16:21:38 +0530

    test

---
+++ src/main/java/com/example/InsertData.java
@@ -0,0 +1,133 @@
+package com.example;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertData
+ */
+public class InsertData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	static String JDBC_DRIVER = "com.mysql.jdbc.GoogleDriver";  
+	static String DB_URL = "";
+	static String USER = "root";
+	static String PASS = "root";
+	static Connection conn = null;
+	static Statement stmt = null;   
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertData() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello SQL!!");	
+		try {
+			
+			Class.forName(JDBC_DRIVER);
+			DB_URL = System.getProperty("ae-cloudsql.cloudsql-database-url");
+
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			
+			String path = InsertData.class.getResource("/sample_data.xlsx").getPath();
+			FileInputStream excelFile = new FileInputStream(new File(path));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+            
+            String[] headers = new String[55];
+            String[] cRow = new String[55];
+            boolean firstRow = true ;
+            int index = 0;
+           
+            while (iterator.hasNext()) {
+            	
+            	index = 0;
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+                	
+                    Cell currentCell = cellIterator.next();
+                    //getCellTypeEnum shown as deprecated for version 3.15
+                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                    	
+                        //System.out.print(currentCell.getStringCellValue() + "--");
+                        if(firstRow){
+                        	headers[index] = currentCell.getStringCellValue();
+                        	//System.out.println(headers[index]);
+                        	index++;
+                        }
+                        else{
+                        	cRow[index] = currentCell.getStringCellValue();
+                        	index++;
+                        }
+                     
+                        
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                    	
+                        //System.out.print(currentCell.getNumericCellValue() + "--");
+                        if(firstRow){
+                            headers[index] = currentCell.getStringCellValue();
+                            index++;
+                        }
+                        else{
+                        	cRow[index] = currentCell.getStringCellValue();
+                        	index++;
+                        }
+                    }
+                }
+               firstRow = false;
+               //System.out.println(cRow[0]);
+               insertTopic(conn,cRow[0]);
+            }
+            
+            /*for (int i = 0; i < headers.length; i++) {
+				System.out.println(headers[i]);
+			}
+            System.out.println("colsize : "+headers.length);*/
+			
+		} catch (Exception e) {
+			System.out.println("exception!!");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	public void insertTopic(Connection conn, String topic) throws SQLException {
+		stmt = conn.createStatement();
+		int t = stmt.executeUpdate("insert into Topics(topic_name) Values('"+topic+"')");
+	}
+}
