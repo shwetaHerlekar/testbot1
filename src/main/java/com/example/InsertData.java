b38605d14870826a1a9037f0fbbb96acbaa46d80
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-31 18:03:50 +0530

    test

--- src/main/java/com/example/InsertData.java
+++ src/main/java/com/example/InsertData.java
@@ -28,8 +28,8 @@ public class InsertData extends HttpServlet {
 	static String DB_URL = "";
 	static String USER = "root";
 	static String PASS = "root";
-	static Connection conn = null;
-	static Statement stmt = null;   
+	//static Connection conn = null;
+	//static Statement stmt = null;   
 	public int law_id=0;
 	
        
@@ -117,7 +117,7 @@ public class InsertData extends HttpServlet {
                 	if(RowCount<4){
                 		insertLawDesc(headers, cRow, out);
                 	}
-             	   //insertQuestion(conn, cRow[2], cRow[0], cRow[1], out);
+             	   //insertQuestion(cRow[2], cRow[0], cRow[1], out);
                 }
                 else
                 {
@@ -145,23 +145,29 @@ public class InsertData extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
-	public void insertTopic(String topic) throws SQLException {
-		stmt = conn.createStatement();
+	public void insertTopic(String topic) throws SQLException, ClassNotFoundException {
+		Connection conn1 = createDBConnection();
+		Statement stmt = conn1.createStatement();
 		int t = stmt.executeUpdate("insert into Topics(topic_name) Values('"+topic+"')");
+		conn1.close();
+		stmt = null;
 	}
 	
 	public void insertSubTopic(String subtopic, String topic,PrintWriter out) throws SQLException, ClassNotFoundException {
-		stmt = conn.createStatement();
+		Connection conn1 = createDBConnection();
+		Statement stmt = conn1.createStatement();
 		int topic_id = getTopicId(topic, out);
 		//out.println(subtopic);
 		int t = stmt.executeUpdate("insert into SubTopics(sub_topic_name,topic_id) Values('"+subtopic+"','"+topic_id+"')");
+		conn1.close();
+		stmt = null;
 	}
 	
 	public int getTopicId(String topic, PrintWriter out) throws SQLException, ClassNotFoundException{
 		
 		out.println(topic);
 		Connection conn1 = createDBConnection();
-		stmt = conn1.createStatement();
+		Statement stmt = conn1.createStatement();
 		ResultSet rs = stmt.executeQuery("select topic_id from Topics where topic_name='"+topic+"';");
 		int id=-1;
 		while(rs.next()){
@@ -175,8 +181,9 @@ public class InsertData extends HttpServlet {
 		return id;
 	}
 	
-	public void insertState(String[] headers, String country,PrintWriter out) throws SQLException {
-		stmt = conn.createStatement();
+	public void insertState(String[] headers, String country,PrintWriter out) throws SQLException, ClassNotFoundException {
+		Connection conn1 = createDBConnection();
+		Statement stmt = conn1.createStatement();
 		out.println("inside state");
 		
 		for (int i = 5; i < headers.length; i++) {
@@ -184,8 +191,8 @@ public class InsertData extends HttpServlet {
 			int t1 = 1;
 			int t = stmt.executeUpdate("insert into State(state_name,country_id) Values('"+headers[i]+"','"+t1+"')");
 		}
-		
-		
+		conn1.close();
+		stmt = null;
 	}
 	
 	public void insertLawDesc(String[] headers, String[] curRow,PrintWriter out) throws SQLException, ClassNotFoundException {
@@ -203,9 +210,10 @@ public class InsertData extends HttpServlet {
 				int id = 1;
 				int id1= getTopicId(curRow[0], out);
 				Connection conn1 = createDBConnection();
-				stmt = conn1.createStatement();
+				Statement stmt = conn1.createStatement();
 				int t = stmt.executeUpdate("insert into Law_Description(law_description,country_id,topic_id) Values('"+curRow[i]+"','"+id+"','"+id1+"')");
 				conn1.close();
+				stmt = null;
 			}
 			else
 			{
@@ -213,24 +221,27 @@ public class InsertData extends HttpServlet {
 				int id1 = getstateId(headers[i], out);
 				int id2 = getTopicId(curRow[0], out);
 				Connection conn1 = createDBConnection();
-				stmt = conn1.createStatement();
+				Statement stmt = conn1.createStatement();
 				int t = stmt.executeUpdate("insert into Law_Description(law_description,state_id,country_id,topic_id) Values('"+curRow[i]+"','"+id1+"','"+id+"','"+id2+"')");
 				conn1.close();
+				stmt = null;
 			}
 		}
-		conn.close();
     }
 	
 	
-	public int getstateId(String state, PrintWriter out) throws SQLException{
+	public int getstateId(String state, PrintWriter out) throws SQLException, ClassNotFoundException{
 		out.println(state);
-		stmt = conn.createStatement();
+		Connection conn1 = createDBConnection();
+		Statement stmt = conn1.createStatement();
 		ResultSet rs = stmt.executeQuery("select state_id from State where state_name='"+state+"';");
 		int id=-1;
 		while(rs.next()){
 	         //Retrieve by column name
 	         id  = rs.getInt("state_id");
 	         out.println("subtopicid"+id);
+	         conn1.close();
+	         stmt = null;
 	         return id;
 	      }
 		return id;
@@ -239,7 +250,7 @@ public class InsertData extends HttpServlet {
 	public int getSubTopicId(String subtopic, PrintWriter out) throws SQLException, ClassNotFoundException{
 		//out.println(subtopic);
 		Connection conn1 = createDBConnection();
-		stmt = conn1.createStatement();
+		Statement stmt = conn1.createStatement();
 		ResultSet rs = stmt.executeQuery("select sub_topic_id from SubTopics where sub_topic_name='"+subtopic+"';");
 		int id=-1;
 		while(rs.next()){
@@ -253,16 +264,19 @@ public class InsertData extends HttpServlet {
 		return id;
 	}
 	
-	public void insertQuestion(Connection conn, String question, String topic,String subtopic,PrintWriter out) throws SQLException, ClassNotFoundException {
+	public void insertQuestion(String question, String topic,String subtopic,PrintWriter out) throws SQLException, ClassNotFoundException {
 		question = question.replaceAll("\'", "");
+		Connection conn1 = createDBConnection();
 		out.println(question);
 		int topic_id = getTopicId(topic, out);
 		int sub_topic_id = getSubTopicId(subtopic, out);
-		stmt = conn.createStatement();
+		Statement stmt = conn1.createStatement();
 		int uid = 1;
 		String sql = "insert into QuestionsMgnt(possible_questions,questions_type,User_id,topic_id,sub_topic_id) Values('"+question+"','SYSTEM','"+uid+"','"+topic_id+"','"+sub_topic_id+"')";
 		//out.println(sql);
 		int t = stmt.executeUpdate(sql);	
+		conn1.close();
+		stmt = null;
 	}
 	
 	public Connection createDBConnection() throws ClassNotFoundException, SQLException{
