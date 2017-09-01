f0c727a70bbb3a94730603569e2bd89fe1a7c814
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-09-01 18:41:05 +0530

    test

--- src/main/java/com/example/InsertData.java
+++ src/main/java/com/example/InsertData.java
@@ -32,8 +32,8 @@ public class InsertData extends HttpServlet {
 	static String DB_URL = "";
 	static String USER = "root";
 	static String PASS = "root";
-	//static Connection conn = null;
-	//static Statement stmt = null;   
+	static Connection conn = null;
+	static Statement stmt = null;   
 	public int law_id=0;
 	
        
@@ -54,13 +54,9 @@ public class InsertData extends HttpServlet {
 		out.println("Hello SQL!!");	
 		try {
 			
-			/*Class.forName(JDBC_DRIVER);
-			DB_URL = System.getProperty("ae-cloudsql.cloudsql-database-url");
-
-			//System.out.println("Connecting to a selected database...");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);*/
-			
-			//conn = createDBConnection();
+		
+			conn = createDBConnection();
+			stmt = conn.createStatement();
 			
 			String path = InsertData.class.getResource("/sample_data.xlsx").getPath();
 			FileInputStream excelFile = new FileInputStream(new File(path));
@@ -76,7 +72,9 @@ public class InsertData extends HttpServlet {
            
             while (iterator.hasNext()) {
             	
-            	log.info("row count :"+RowCount);
+            	RowCount++;
+            	
+            	log.info("inside new row :"+RowCount);
             	
             	index = 0;
                 Row currentRow = iterator.next();
@@ -119,11 +117,8 @@ public class InsertData extends HttpServlet {
              	   //insertTopic(cRow[0]);
              	   //insertSubTopic(cRow[1], cRow[0], out);
              	   
-                	if(RowCount<2)
-                	{
-                		insertLawDesc(headers, cRow, out);
-                		RowCount++;
-                	}
+                	insertLawDesc(headers, cRow, out);
+                	
                 	
              	   //insertQuestion(cRow[2], cRow[0], cRow[1], out);
                 }
@@ -135,7 +130,8 @@ public class InsertData extends HttpServlet {
                firstRow = false;
                
             }
-            
+            stmt.close();
+            conn.close();
 			
 		} catch (Exception e) {
 			out.print("exception!!");
@@ -154,21 +150,17 @@ public class InsertData extends HttpServlet {
 	}
 
 	public void insertTopic(String topic) throws SQLException, ClassNotFoundException {
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+
 		int t = stmt.executeUpdate("insert into Topics(topic_name) Values('"+topic+"')");
-		stmt.close();
-		conn1.close();
 		
 	}
 	
 	public void insertSubTopic(String subtopic, String topic,PrintWriter out) throws SQLException, ClassNotFoundException {
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+		
 		int topic_id = getTopicId(topic, out);
 		//out.println(subtopic);
 		int t = stmt.executeUpdate("insert into SubTopics(sub_topic_name,topic_id) Values('"+subtopic+"','"+topic_id+"')");
-		conn1.close();
+		
 		
 	}
 	
@@ -176,8 +168,7 @@ public class InsertData extends HttpServlet {
 		
 		//out.println(topic);
 		//log.info(topic);
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+		
 		ResultSet rs = stmt.executeQuery("select topic_id from Topics where topic_name='"+topic+"';");
 		int id=-1;
 		while(rs.next()){
@@ -185,16 +176,14 @@ public class InsertData extends HttpServlet {
 	         id  = rs.getInt("topic_id");
 	        //out.println("topic id:"+id);
 	        rs.close();
-	        stmt.close();
-	        conn1.close();
+	        
 	         return id;
 	      }
 		return id;
 	}
 	
 	public void insertState(String[] headers, String country,PrintWriter out) throws SQLException, ClassNotFoundException {
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+		
 		out.println("inside state");
 		
 		for (int i = 5; i < headers.length; i++) {
@@ -202,8 +191,7 @@ public class InsertData extends HttpServlet {
 			int t1 = 1;
 			int t = stmt.executeUpdate("insert into State(state_name,country_id) Values('"+headers[i]+"','"+t1+"')");
 		}
-		conn1.close();
-		stmt = null;
+		
 	}
 	
 	public void insertLawDesc(String[] headers, String[] curRow,PrintWriter out) throws SQLException, ClassNotFoundException {
@@ -223,11 +211,9 @@ public class InsertData extends HttpServlet {
 				log.info("i :"+i);
 				int id = 1;
 				int id1= getSubTopicId(curRow[1], out);
-				Connection conn1 = createDBConnection();
-				Statement stmt = conn1.createStatement();
+			
 				int t = stmt.executeUpdate("insert into Law_Description(law_description,country_id,sub_topic_id) Values('"+curRow[i]+"','"+id+"','"+id1+"')");
-				stmt.close();;
-				conn1.close();
+				
 				
 			}
 			else
@@ -237,11 +223,9 @@ public class InsertData extends HttpServlet {
 				int id2 = getSubTopicId(curRow[1], out);
 				log.info("i :"+i);
 				//log.info("id2:"+id2);
-				Connection conn1 = createDBConnection();
-				Statement stmt = conn1.createStatement();
+				
 				int t = stmt.executeUpdate("insert into Law_Description(law_description,state_id,country_id,sub_topic_id) Values('"+curRow[i]+"','"+id1+"','"+id+"','"+id2+"')");
-				stmt.close();
-				conn1.close();
+				
 				
 			}
 		}
@@ -251,8 +235,7 @@ public class InsertData extends HttpServlet {
 	public int getstateId(String state, PrintWriter out) throws SQLException, ClassNotFoundException{
 		//out.println(state);
 		log.info(state);
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+		
 		ResultSet rs = stmt.executeQuery("select state_id from State where state_name='"+state+"';");
 		int id=-1;
 		while(rs.next()){
@@ -260,8 +243,7 @@ public class InsertData extends HttpServlet {
 	         id  = rs.getInt("state_id");
 	         //out.println("subtopicid"+id);
 	         rs.close();
-		     stmt.close();
-	         conn1.close();
+		   
 	         return id;
 	      }
 		return id;
@@ -269,8 +251,7 @@ public class InsertData extends HttpServlet {
 	
 	public int getSubTopicId(String subtopic, PrintWriter out) throws SQLException, ClassNotFoundException{
 		//out.println(subtopic);
-		Connection conn1 = createDBConnection();
-		Statement stmt = conn1.createStatement();
+		
 		ResultSet rs = stmt.executeQuery("select sub_topic_id from SubTopics where sub_topic_name='"+subtopic+"';");
 		int id=-1;
 		while(rs.next()){
@@ -278,8 +259,7 @@ public class InsertData extends HttpServlet {
 	         id  = rs.getInt("sub_topic_id");
 	         //out.println(id);
 	         rs.close();
-		 stmt.close();
-	         conn1.close();
+	
 	         return id;
 	      }
 		return id;
@@ -287,17 +267,16 @@ public class InsertData extends HttpServlet {
 	
 	public void insertQuestion(String question, String topic,String subtopic,PrintWriter out) throws SQLException, ClassNotFoundException {
 		question = question.replaceAll("\'", "");
-		Connection conn1 = createDBConnection();
+
 		out.println(question);
 		int topic_id = getTopicId(topic, out);
 		int sub_topic_id = getSubTopicId(subtopic, out);
-		Statement stmt = conn1.createStatement();
+		
 		int uid = 1;
 		String sql = "insert into QuestionsMgnt(possible_questions,questions_type,User_id,topic_id,sub_topic_id) Values('"+question+"','SYSTEM','"+uid+"','"+topic_id+"','"+sub_topic_id+"')";
 		//out.println(sql);
 		int t = stmt.executeUpdate(sql);	
-		conn1.close();
-		stmt = null;
+		
 	}
 	
 	public Connection createDBConnection() throws ClassNotFoundException, SQLException{
